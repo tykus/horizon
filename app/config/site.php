bb4f73cf945d7bfbc57d3f24b953d612175ecfb4
Brian O'Sullivan (osx) <brian@tykus.ie>
2014-08-07 00:42:10 +0800

    Adding a configuration file with app-specific details

---
+++ app/config/site.php
@@ -0,0 +1,29 @@
+<?php
+
+return array(
+
+	'app_name' => 'Horizon',
+	'business_name' => 'Horizon Centre',
+	'business_address' => 'Denshaw House,
+												 121 Baggot Street Lower, 
+												 Dublin 2',
+	'business_location' => array(
+		'latitude' => 53.3372335,
+		'longitude' => -6.2501318
+	),
+	'telephone' => '01-234-5678',
+	'mobile' => '087-654-3210',
+	'url' => 'http://www.horizoncentre.ie',
+	'email' => 'robert@horizoncentre.ie',
+
+	'description' => '',
+	'keywords' => '',
+	'slogan' => 'Inspiring positive change.',
+	'logo_path' => 'img/logo.png',
+	'logo_small_path' => 'img/logo_small.png',
+
+	'author' => 'Tykus Web Development',
+	'author_url' => 'http://www.tykus.ie',
+	'author_email' => 'brian@tykus.ie',
+
+);
