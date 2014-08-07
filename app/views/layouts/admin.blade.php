77eec02015b3540ab974f8dff78f6fc1d32edef3
Brian O'Sullivan <brian@tykus.ie>
2014-08-07 11:49:49 +0800

    Updating assets

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -85,6 +85,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/admin.min.js"></script>
+    <script src="js/admin.js"></script>
   </body>
 </html>
