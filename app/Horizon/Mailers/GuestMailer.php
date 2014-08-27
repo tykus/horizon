c225b999c2ac412604496c14d3595bba67f26efc
Brian O'Sullivan <brian@tykus.ie>
2014-08-28 03:58:37 +0800

    Fixing email subject in GuestMailer class

--- app/Horizon/Mailers/GuestMailer.php
+++ app/Horizon/Mailers/GuestMailer.php
@@ -7,7 +7,7 @@ class GuestMailer extends Mailer {
   public function reply(Guest $guest, $data)
   {
     $view = 'emails.reply';
-    $subject = $data['subject'] || "Thank you for your enquiry.";
+    $subject = $data['subject'];
     $viewData = $data;
 
     return $this->sendTo($guest, $subject, $view, $viewData);
