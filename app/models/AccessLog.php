1fa7dd55aec3611a54e66c51949b67c0732d4985
Brian O'Sullivan <brian@tykus.ie>
2014-08-28 06:38:06 +0800

    Adding fillable properties to model

--- app/models/AccessLog.php
+++ app/models/AccessLog.php
@@ -1,5 +1,5 @@
 <?php
 
 class AccessLog extends \Eloquent {
-	protected $fillable = [];
+	protected $fillable = ['email','login_success','ip','country_code','country_name','region_code','region_name','city','zipcode','latitude','longitude','metro_code','area_code'];
 }
