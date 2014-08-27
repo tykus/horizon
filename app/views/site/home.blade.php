c5ebb6a27abd86c48332c3e04bfd9a0f24af3853
Brian O'Sullivan <brian@tykus.ie>
2014-08-27 22:57:25 +0800

    Tidying up views directory

--- app/views/site/home.blade.php
+++ app/views/site/home.blade.php
@@ -3,16 +3,16 @@
 @section('content')
     <!-- Intro -->
     <div id="home" class="intro-header">
-      @include('home._intro')
+      @include('site.home._intro')
     </div>
 
     <!-- Page Content -->
     <div id="services" class="content-services">
-        @include('home._services')
+        @include('site.home._services')
     </div>
 
     <div id="about" class="content-about">
-      @include('home._about')
+      @include('site.home._about')
     </div>
 
     <div id="location">
@@ -20,6 +20,6 @@
     </div>
 
     <div id="contact" class="content-contact">
-      @include('home._contact')
+      @include('site.home._contact')
     </div>
 @stop
