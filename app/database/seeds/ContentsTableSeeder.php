730fc7c22987cf28bfba83da676993d5731261c0
Brian <brian@tykus.ie>
2014-08-31 17:46:32 +0800

    Updating Seed files

--- app/database/seeds/ContentsTableSeeder.php
+++ app/database/seeds/ContentsTableSeeder.php
@@ -2,9 +2,9 @@
 
 class ContentsTableSeeder extends Seeder {
 
-	public function run()
-	{
-		$about = "<p><strong>Robert Gill — Dip. Psych., M.I.A.C.P.</strong> is an integrative
+  public function run()
+  {
+    $about = "<p><strong>Robert Gill — Dip. Psych., M.I.A.C.P.</strong> is an integrative
       psychotherapist/counsellor based in Dublin city centre.</p>
       <p>Degree in Integrative Psychotherapy and Counselling. Accredited with the
       Irish Association of Humanistic and Integrative Psychotherapy (IAHIP) and the Irish
--- app/database/seeds/SettingsTableSeeder.php
+++ app/database/seeds/SettingsTableSeeder.php
@@ -4,15 +4,6 @@ class SettingsTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$about = "<p><strong>Robert Gill — Dip. Psych., M.I.A.C.P.</strong> is an integrative
-			psychotherapist/counsellor based in Dublin city centre.</p>
-			<p>Degree in Integrative Psychotherapy and Counselling. Accredited with the
-			Irish Association of Humanistic and Integrative Psychotherapy (IAHIP) and the Irish
-			Association for Counselling and Psychotherapy (IACP). Accredited supervisor with IAHIP.</p>
-			<p>Specialist experience in anxiety, depression, relationships, body-based
-			psychological problems or 'psychosomatic' problems, trauma, bereavement, eating disorders,
-			addictions, anger management, men’s issues, self esteem, among others.</p>
-		";
 		$location = "53.3372335,-6.2501318";
 		$name = 'Horizon Centre';
 		$address = nl2br("Denshaw House,\n121 Baggot Street Lower,\nDublin 2");
@@ -21,12 +12,6 @@ class SettingsTableSeeder extends Seeder {
 		$mobile = '087-654-3210';
 		$slogan = 'Inspiring positive change';
 
-			Setting::create([
-				'key' => 'about',
-				'value' => $about,
-				'field' =>'textarea',
-				'on_menu' => 0
-			]);
 			Setting::create([
 				'key' => 'location',
 				'value' => $location,
