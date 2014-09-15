589c6ff724c544e7636b25e581d423cf7f6d3a0c
Brian O'Sullivan (osx) <brian@tykus.ie>
2014-09-15 21:44:09 +0800

    Updating API to fake a response when using local env

--- app/Horizon/Utilities/Api.php
+++ app/Horizon/Utilities/Api.php
@@ -1,12 +1,23 @@
 <?php namespace Horizon\Utilities;
 
+use \App;
+use \Request;
 use \Response;
 
 class Api {
 
   public function request($url="")
   {
-    $data = file_get_contents($url);
+  	# Need to fake data in development enviroment
+  	if (App::environment() == 'local')
+  	{
+    	$data = json_encode(['country_code'=>'CN', 'city'=>'Dalian']); 
+  	}
+  	else
+  	{
+    	$data = file_get_contents($url);
+    }
+
     $response = Response::make($data, 200);
     $response->headers->set('Content-Type', 'application/json');
 
