36a552f61c96d8cfd4d5cd1c0a07efaad05437b3
Brian <brian@tykus.ie>
2014-09-22 03:00:40 +0800

    Changing request to cURL because fopen wrapper is disabled on production

--- app/Horizon/Utilities/Api.php
+++ app/Horizon/Utilities/Api.php
@@ -11,11 +11,27 @@ class Api {
   	# Need to fake data in development enviroment
   	if (App::environment() == 'local')
   	{
-    	$data = json_encode(['country_code'=>'CN', 'city'=>'Dalian']); 
+    	$data = json_encode(['country_code'=>'CN', 'city'=>'Dalian']);
   	}
   	else
   	{
-    	$data = file_get_contents($url);
+      $ch = curl_init();
+      curl_setopt ($ch, CURLOPT_URL, $url);
+      curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+      curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+      $data = curl_exec($ch);
+      if (curl_errno($ch)) {
+        echo curl_error($ch);
+        echo "\n<br />";
+        $data = '';
+      } else {
+        curl_close($ch);
+      }
+
+      if (!is_string($data) || !strlen($data)) {
+        $data = json_encode(['country_code'=>'CN', 'city'=>'Dalian']); # Fallback
+      }
+
     }
 
     $response = Response::make($data, 200);
