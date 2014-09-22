4a8e354b7b895b29fa35b645fd74a308174cc859
Brian <brian@tykus.ie>
2014-09-22 15:40:53 +0800

    Strip tags from the services panel

--- app/views/site/services/_panel.blade.php
+++ app/views/site/services/_panel.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-4 service">
   <img class="img-circle" src="{{ $service->image_path }}">
   <h3>{{{ $service->title }}}</h3>
-  <p>{{{ $service->introduction }}}</p>
+  <p>{{ strip_tags($service->introduction) }}</p>
   <p>
     {{ HTML::linkRoute('service_path', 'More »', $service->slug, ['class'=>'btn btn-default pull-right']) }}
   </p>
