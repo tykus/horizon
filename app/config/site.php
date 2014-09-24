43444c56f2d6e7e3f73f95b67cc6c9ed6b934d1a
Brian <brian@tykus.ie>
2014-09-25 02:55:33 +0800

    Tidying up the site config file

--- app/config/site.php
+++ app/config/site.php
@@ -13,17 +13,4 @@ return array(
 	'author' => 'Tykus Web Development',
 	'author_url' => 'http://www.tykus.ie',
 	'author_email' => 'brian@tykus.ie',
-
-	// // Business-specific Information
-	// 'business' => array(
-	// 	'latitude' => 53.3372335,
-	// 	'longitude' => -6.2501318,
-	// 	'name' => 'Horizon Centre',
-	// 	'address' => nl2br("Denshaw House,\n121 Baggot Street Lower,\nDublin 2"),
-	// 	'email' => 'robert@horizoncentre.ie',
-	// 	'phone' => '087-654-3210',
-	// 	'mobile' => '087-654-3210',
-	// 	'url' => 'http://www.horizoncentre.ie',
-	// 	'slogan' => 'Inspiring positive change.',
-	// )
 );
