e49f806fcc8f5ad33c31241189448b69aa6d1a18
Brian <brian@tykus.ie>
2014-09-25 00:36:14 +0800

    Removing cumbersome property name translation 'message' => 'body'

--- app/Horizon/Mailers/UserMailer.php
+++ app/Horizon/Mailers/UserMailer.php
@@ -9,20 +9,9 @@ class UserMailer extends Mailer {
   public function forward(User $user, $data)
   {
     $view = 'emails.forward';
-    $this->setupViewData($data);
+    $subject = "Website Enquiry - {$data['name']}";
 
-    return $this->sendTo($user, $this->view_data['subject'], $view, $this->view_data);
-  }
-
-  /**
-   * setupViewData()
-   */
-  private function setupViewData($data)
-  {
-    $data['body'] = $data['message']; # var name collision in the Mailer class
-    unset($data['message']);
-    $data['subject'] = "Enquiry received thru website.";
-    $this->view_data = $data;
+    return $this->sendTo($user, $subject, $view, $data);
   }
 
 }
